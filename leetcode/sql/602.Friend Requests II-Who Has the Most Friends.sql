96d573732f25e65dd8b8949a3312758d276feda8
limon-7 <liibd7@gmail.com>
2024-11-27 03:42:27 +0600

    Feat: add solution 602.Friend Requests II: Who Has the Most Friends.sql

---
+++ leetcode/sql/602.Friend Requests II-Who Has the Most Friends.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+WITH
+  AllIds AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id FROM RequestAccepted
+  )
+SELECT
+  id,
+  COUNT(*) AS num
+FROM AllIds
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
